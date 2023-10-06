f88ddf1f3c4089800d0cf93eecaa8d7d4191b2da
stulucy <145096018+stulucy@users.noreply.github.com>
2023-10-06 14:39:36 +0900

    Add files via upload

---
+++ hw3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	
+	for (int i = 1; i < 6; i++) {
+		for (int j = 4; j>=i; j--) {
+			printf(" ");
+		}
+		for (int j = 1; j<=i*2-1; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
